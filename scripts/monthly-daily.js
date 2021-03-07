2be4afe9d5518c65870ee97915478e02379e3aea
Mr.LYY <lyulekin.dima@gmail.com>
2021-03-07 15:58:14 +0300

    choose day alert

--- scripts/monthly-daily.js
+++ scripts/monthly-daily.js
@@ -8,28 +8,38 @@ for (let i = 0; i < tabNavs.length; i++) {
 
     const activeDay = document.querySelector('.calendar__day_active')
 
-    if (activeDay.classList.contains('calendar__day_tasked')) {
+    if (activeDay != null) {
+
+      if (activeDay.classList.contains('calendar__day_tasked')) {
         
-      document.querySelector('.daily-tasks__empty').classList.remove('daily-tasks__empty_active');
-             
-    } else {
+        document.querySelector('.daily-tasks__empty').classList.remove('daily-tasks__empty_active');
+               
+      } else {
+  
+        document.querySelector('.daily-tasks__empty').classList.add('daily-tasks__empty_active');
+        
+      }
 
-      document.querySelector('.daily-tasks__empty').classList.add('daily-tasks__empty_active');
-      
-    }
 
-    const activeTabAttr = evt.target.getAttribute("data-tab");
+      const activeTabAttr = evt.target.getAttribute("data-tab");
 
-    for (let j = 0; j < tabNavs.length; j++) {
+      for (let j = 0; j < tabNavs.length; j++) {
       const contentAttr = tabPanes[j].getAttribute("data-tab-content");
 
       if (activeTabAttr === contentAttr) {
-        tabNavs[j].classList.add("switch_active");
-        tabPanes[j].classList.add("tab-content_active"); 
-      } else {
-        tabNavs[j].classList.remove("switch_active");
-        tabPanes[j].classList.remove("tab-content_active");
-      }
-    };
+          tabNavs[j].classList.add("switch_active");
+          tabPanes[j].classList.add("tab-content_active"); 
+        } else {
+          tabNavs[j].classList.remove("switch_active");
+          tabPanes[j].classList.remove("tab-content_active");
+        }
+      };
+
+
+    } else {
+
+      alert('Пожалуйста, выберите день недели!')
+      
+    }
   });
 }
